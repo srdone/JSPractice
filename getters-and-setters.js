cc596714f5c7d304a3eecd6c00f6b3dda13e8413
Stephen Done <sdone@simplifile.com>
2016-07-15 09:17:18 -0600

    getters and setters

---
+++ getters-and-setters.js
@@ -0,0 +1,14 @@
+var myObject = {
+  get a() {
+    return this._a_;
+  },
+  set a(val) {
+    this._a_ = val * 2;
+  }
+}
+
+myObject.a = 2;
+
+console.log(myObject);
+console.log(myObject.a);
+console.log(myObject._a_);
